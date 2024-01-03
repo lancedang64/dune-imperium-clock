10f13c91e0c84809e62cbd964263f088d68806f6
Lance Dang <lancedang64@gmail.com>
2024-01-03 20:52:10 +1100

    Add GameView component

---
+++ src/components/GameView/GameView.styles.ts
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    component: {
+        height: '100%',
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        backgroundColor: '#fff',
+        padding: 30,
+    },
+});
---
+++ src/components/GameView/GameView.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import { styles } from './GameView.styles';
+
+interface ComponentProps {}
+
+export const GameView = (props: ComponentProps) => {
+    return <View style={styles.component}>TemplateComponent</View>;
+};
